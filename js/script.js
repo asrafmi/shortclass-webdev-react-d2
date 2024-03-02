484f3072a95547d65fc83f10da2bf39b0d7ba60f
asrafmi <asraf.muhammad07@gmail.com>
2024-03-02 14:13:02 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('Hello, World!');
+
+var bootcamp = 'Productzilla';
+var bootcamp = 'Academy';
+let durasi = 1;
+durasi = 20;
+const peserta = 100;
+
+console.log('var => ', bootcamp);
+console.log('let => ', durasi);
+console.log('const yang ada di luar block => ', peserta);
